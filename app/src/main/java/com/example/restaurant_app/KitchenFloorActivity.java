c094678ba336d3d88751016c3d2d05aa30bda5c2
Andreas Å Miun <andreaa.asenlund@gmail.com>
2024-12-10 21:26:35 +0100

    kitchen_view

--- app/src/main/java/com/example/restaurant_app/KitchenFloorActivity.java
+++ app/src/main/java/com/example/restaurant_app/KitchenFloorActivity.java
@@ -3,8 +3,6 @@ package com.example.restaurant_app;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 import android.widget.Button;
 
 
--- app/src/main/java/com/example/restaurant_app/MainActivity.java
+++ app/src/main/java/com/example/restaurant_app/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Your code heretest
+        // Your code here
     }
 }
