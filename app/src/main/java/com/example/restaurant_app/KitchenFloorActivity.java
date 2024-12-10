23ec34773b35d34926cc6937271a41a2af7710e3
Andreas Å Miun <andreaa.asenlund@gmail.com>
2024-12-10 20:18:36 +0100

    Val av arbete

---
+++ app/src/main/java/com/example/restaurant_app/KitchenFloorActivity.java
@@ -0,0 +1,28 @@
+package com.example.restaurant_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+
+public class KitchenFloorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kitchen_floor);
+
+        Button pickWorkButton = findViewById(R.id.kitchenButton);
+
+        pickWorkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KitchenFloorActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
