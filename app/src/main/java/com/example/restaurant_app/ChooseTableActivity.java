c26ae12a3c902fa1b9cd50557dab74b9e2a2c697
frpa2201 <fredrikpaulsson03@gmail.com>
2024-12-11 01:12:11 +0100

    framework för popup ruta

--- app/src/main/java/com/example/restaurant_app/ChooseTableActivity.java
+++ app/src/main/java/com/example/restaurant_app/ChooseTableActivity.java
@@ -1,6 +1,12 @@
 package com.example.restaurant_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +18,7 @@ public class ChooseTableActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_choose_table);
@@ -20,6 +27,19 @@ public class ChooseTableActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        Button createOrder = findViewById(R.id.addOrder);
+        createOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ChooseTableActivity.this, tablePopup.class));
+            }
+        });
+
+
+
     }
 
 
