3adadc910f3913d593e57806963013e836572cd2
Andreas Å Miun <andreaa.asenlund@gmail.com>
2024-12-12 15:02:48 +0100

    Merge av valAvBord och orderHantering samt att det går att ta sig från valAvBord till orderHantering

--- app/src/main/java/com/example/restaurant_app/tablePopup.java
+++ app/src/main/java/com/example/restaurant_app/tablePopup.java
@@ -1,8 +1,10 @@
 package com.example.restaurant_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +18,6 @@ public class tablePopup extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.table_popup);
 
         DisplayMetrics dm = new DisplayMetrics();
@@ -29,5 +28,28 @@ public class tablePopup extends Activity {
 
         getWindow().setLayout((int)(width*.7),(int)(height*.6));
 
+
+        setupTableChoice(findViewById(android.R.id.content));
+
+    }
+
+    public void setupTableChoice(View rootView){
+        View table1View = rootView.findViewById(R.id.viewButton1);
+        View table2View = rootView.findViewById(R.id.viewButton2);
+        View table3View = rootView.findViewById(R.id.viewButton3);
+        View table4View = rootView.findViewById(R.id.viewButton4);
+        View table5View = rootView.findViewById(R.id.viewButton5);
+        View table6View = rootView.findViewById(R.id.viewButton6);
+        View table7View = rootView.findViewById(R.id.viewButton7);
+        View table8View = rootView.findViewById(R.id.viewButton8);
+        View table9View = rootView.findViewById(R.id.viewButton9);
+        View table10View = rootView.findViewById(R.id.viewButton10);
+
+        table1View.setOnClickListener(view -> showOrderActivity());
+    }
+
+    private void showOrderActivity(){
+        Intent chooseTableIntent = new Intent(tablePopup.this, MainActivity.class);
+        startActivity(chooseTableIntent);
     }
 }
